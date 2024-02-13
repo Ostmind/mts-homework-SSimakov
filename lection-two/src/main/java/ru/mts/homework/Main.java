5f8697b0e0ca6e0c3479d11452bdc548a1348001
sergey <serz.sim@gmail.com>
2024-02-13 18:13:36 +0300

    New Repo

---
+++ lection-two/src/main/java/ru/mts/homework/Main.java
@@ -0,0 +1,12 @@
+package ru.mts.homework;
+
+
+public class Main{
+    public static void sayHello(){
+        System.out.println("Hello world!");
+    }
+
+    public static void main(String [] args){
+        sayHello();
+    }
+}
